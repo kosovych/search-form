81de583a6d0a14c8eb1449ac8a3a4b68c6bfe40e
Incode15 <bassicinbox@gmail.com>
2018-02-19 17:34:40 +0200

    90% finish layout 2

--- src/js/main.js
+++ src/js/main.js
@@ -38,13 +38,13 @@ $('.hotel-travellers').on('click', function (ev) {
 });
 
 $('.flight-tab').on('click', function () {
-	$('.search').css('backgroundImage', 'url(../img/bg-search.png)');
+	$('.search').css('backgroundImage', 'url(./img/bg-search.png)');
 });
 
 $('.cruise-tab').on('click', function () {
-	$('.search').css('backgroundImage', 'url(../img/bg-advantage.png)');
+	$('.search').css('backgroundImage', 'url(./img/bg-advantage.png)');
 });
 
 $('.hotel-tab').on('click', function () {
-	$('.search').css('backgroundImage', 'url(../img/bg-hotel.png)');
+	$('.search').css('backgroundImage', 'url(./img/bg-hotel.png)');
 });
